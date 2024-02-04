3a3737c3e88463372fc281df66c4e056882ef89e
ir32 <alamirfan651@gmail.com>
2024-02-04 20:20:52 +0530

    Razor Payment

--- app/controllers/careerController.js
+++ app/controllers/careerController.js
@@ -1,4 +1,5 @@
 const CareerModel = require('../models/careerModel');
+const Razorpay = require('razorpay');
 
 class CareerController {
     async dreamJob(req, res) {
@@ -11,6 +12,40 @@ class CareerController {
             res.status(500).json({ message: 'Server error' });
         }
     }
-}
+    async initiateRazorpayPayment(req, res) {
+        try {
+          const razorpay = new Razorpay({
+            key_id: 'rzp_test_sQIk3nO8AAvuGJ',
+            key_secret: 'ycpPbAbXrfsA7EuGMDvvwZEi'
+          });
+    
+          const { amount } = req.body; // Get the amount from the request body
+
+          const orderOptions = {
+                amount: parseInt(amount) * 100, // Convert amount to paise (1 INR = 100 paise)
+                currency: 'INR',
+                receipt: 'order_rcptid_11',
+                payment_capture: 1 // Auto capture payment
+          };
+    
+          const order = await razorpay.orders.create(orderOptions);
+    
+          const customerDetails = req.body;
+          customerDetails.orderId = order.id; // Add order ID to customer details
+    
+          // Render a payment page or return the order details to the client
+          res.json({
+            orderId: order.id,
+            amount: orderOptions.amount,
+            currency: orderOptions.currency,
+            key_id: razorpay.key_id,
+            customerDetails: customerDetails
+          });
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: 'Error initiating Razorpay payment' });
+        }
+      }
+    }
 
 module.exports = CareerController;
